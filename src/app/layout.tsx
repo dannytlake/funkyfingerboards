d69737dd49a14547c8941f1fe4139fe3fdffeffb
danny lake <danny.lake@orium.com>
2025-03-26 20:52:09 -0700

    feat: setup vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navigation from "./components/Navigation";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -33,6 +34,7 @@ export default function RootLayout({
       <body className={`${inter.className}`}>
         <Navigation />
         <div className="pt-16">{children}</div>
+        <Analytics />
       </body>
     </html>
   );
